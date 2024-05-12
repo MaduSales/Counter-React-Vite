57ce9adfaf0faa7879c244885f29c5ee1ca2a852
Maria Eduarda <maria.mirandadev@gmail.com>
2024-05-11 21:50:30 -0300

    repositorio dois

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Contador from './components/Contador/Contador'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="App">
+      <Contador/>
+    </div>
+  )
+}
+
